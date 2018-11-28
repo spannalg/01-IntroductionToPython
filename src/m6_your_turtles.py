834f858d164f5b11d6aea6e3c0e2173efa682943
spannalg <spannalg@rose-hulman.edu>
2018-11-27 19:25:44 -0500

    DONE

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -5,10 +5,9 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Aaron Wilkin, their colleagues, and Luke Spannan
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
-
 ########################################################################
 # TODO: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
@@ -28,3 +27,24 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+turtle_one = rg.SimpleTurtle('turtle')
+turtle_one.pen = rg.Pen('Red',5)
+turtle_one.speed = 15
+size = 120
+for k in range(12):
+    turtle_one.draw_circle(size)
+    turtle_one.pen_up()
+    turtle_one.right(30)
+    turtle_one.pen_down()
+    size = size - 5
+turtle_two = rg.SimpleTurtle('turtle')
+turtle_two_pen = rg.Pen('Green',10)
+turtle_two.forward(5)
+turtle_two.speed = 10
+for k in range(8):
+    turtle_two.draw_square(200)
+    turtle_two.pen_up()
+    turtle_two.left(45)
+    turtle_two.pen_down()
